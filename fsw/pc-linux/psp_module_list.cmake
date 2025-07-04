8c719cce8b924f6fa78eeae8eccd4c1a7a2046ac
Avi Weiss <thnkslprpt@gmail.com>
2025-07-04 18:30:59 +0200

    test

--- fsw/pc-linux/psp_module_list.cmake
+++ fsw/pc-linux/psp_module_list.cmake
@@ -1,11 +1,15 @@
 # This is a list of modules that is included as a fixed/base set
 # when this PSP is selected.  They must exist under fsw/modules
+#
+# For EVS output port routing:
+#   eventport_printf - Default implementation, outputs to console via OS_printf
+#   eventport_syslog - Linux syslog implementation with priority-based routing
 
 soft_timebase
 timebase_posix_clock
 eeprom_mmap_file
 ram_notimpl
 port_notimpl
-eventport_printf  # Default EVS port implementation (use eventport_syslog for syslog routing)
+eventport_printf
 iodriver
 linux_sysmon
